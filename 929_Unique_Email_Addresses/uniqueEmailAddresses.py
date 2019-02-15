4965c1a972bf3453ac85f81f57e7b114a770b706
shengjia <shengj@anchnet.com>
2019-02-15 22:38:47 +0800

    fix the uniqueEmailAddress after @

--- 929_Unique_Email_Addresses/uniqueEmailAddresses.py
+++ 929_Unique_Email_Addresses/uniqueEmailAddresses.py
@@ -1,19 +1,8 @@
-def numUniqueEmails(emails):
-    newEmails = []
-    for i in emails:
-        subEmail = ""
-        email = i.split('@')
-        if email[1].count('.') == 1:
-            subE = email[0].split('+')
-            subNoPoint = subE[0].split('.')
-            for j in subNoPoint:
-                subEmail = subEmail + j
-            newEmail = subEmail + "@" + email[1]
-            newEmails.append(newEmail)
-    print(len(newEmails))
-    return len(newEmails)
+c
 
 
+test = ["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "test.email+alex@leetcode.com",
+        "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]
 emails = ["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.uzj+o.f.d@kziczvh.com",
           "r.cyo.g+ng.r.iq@tgsg.z.com", "fg.r.u.uzj+lp.k@kziczvh.com", "r.cyo.g+n.h.e+n.g@tgsg.z.com",
           "fg.r.u.uzj+k+p.j@kziczvh.com", "fg.r.u.uzj+w.y+b@kziczvh.com", "r.cyo.g+x+d.c+f.t@tgsg.z.com",
@@ -22,9 +11,8 @@ emails = ["fg.r.u.uzj+o.pw@kziczvh.com", "r.cyo.g+d.h+b.ja@tgsg.z.com", "fg.r.u.
           "fg.r.u.uzj+mvz@kziczvh.com", "fg.r.u.uzj+taj@kziczvh.com", "fg.r.u.uzj+fek@kziczvh.com"]
 numUniqueEmails(emails)
 
-
-
-# 添加边界
+# add border and don't allow more than 2 points after @
+# such as like this "fg.r.u.uzj+mvz@kzic.zvh.com" is false
 # def numUniqueEmails(emails):
 #     emailsLen = len(emails)
 #     newEmails = []
