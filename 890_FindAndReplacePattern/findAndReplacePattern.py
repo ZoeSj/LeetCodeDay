514a14d2dc5f22cf381dc6c0e94b969548f34927
shengjia <shengj@anchnet.com>
2019-02-16 15:49:50 +0800

    pay attention to setdefault and return []

--- 890_FindAndReplacePattern/findAndReplacePattern.py
+++ 890_FindAndReplacePattern/findAndReplacePattern.py
@@ -2,5 +2,25 @@
 # @Date    : 2019-02-15
 # @Author  : shengjia
 
-words = ["abc", "deq", "mee", "aqq", "dkd", "ccc"]
-pattern = "abb"
+# words = ["abc", "deq", "mee", "aqq", "dkd", "ccc"]
+words = ["badc", "abab", "dddd", "dede", "yyxx"]
+
+# words = ["bab"]
+pattern = "baba"
+
+
+def numForPattern(pattern, words):
+    p = []
+
+    def F(word):
+        m = {}
+        return [m.setdefault(c, len(m)) for c in word]
+
+    for w in words:
+        if F(w) == F(pattern):
+            p.append(w)
+
+    return p
+
+
+print (numForPattern(pattern, words))
