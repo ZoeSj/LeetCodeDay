e3c4a2e8ca84e709e5bec65049ab4218a8848057
shengjia <shengj@anchnet.com>
2019-03-04 09:59:39 +0800

    valid parentheses

---
+++ 20_Valid_Parentheses/validParentheses.py
@@ -0,0 +1,29 @@
+# given a string containing just the characters ,determine if the input string is valid.
+# an input string is valid if:
+# 1. Open brackets must be closed by the same type of brackets
+# 2. Open brackets must be closed in the correct order.
+def isValid(s):
+    stack = []
+    dict = {"]": "[", "}": "{", ")": "("}
+    for char in s:
+        if char in dict.values():
+            stack.append(char)
+        elif char in dict.keys():
+            if stack == [] or dict[char] != stack.pop():
+                return False
+        else:
+            return False
+    return stack == []
+
+
+# about dict
+def aboutDict(s):
+    a = dict(one=1, two=2, three=3)
+    b = {'one': 1, 'two': 2, 'three': 3}
+    c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+    d = dict([('two', 2), ('one', 1), ('three', 3)])
+    e = dict({'three': 3, 'one': 1, 'two': 2})
+    if a == b == c == d == e:
+        return s
+    else:
+        return False
