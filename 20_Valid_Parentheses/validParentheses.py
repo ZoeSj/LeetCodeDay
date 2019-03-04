239552b4412d225b4b9b41749120d73a329c0f81
shengjia <shengj@anchnet.com>
2019-03-04 10:16:11 +0800

    another way but use more funtime

--- 20_Valid_Parentheses/validParentheses.py
+++ 20_Valid_Parentheses/validParentheses.py
@@ -16,14 +16,24 @@ def isValid(s):
     return stack == []
 
 
+input = "([)]"
+isValid(input)
+
+
 # about dict
-def aboutDict(s):
-    a = dict(one=1, two=2, three=3)
-    b = {'one': 1, 'two': 2, 'three': 3}
-    c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
-    d = dict([('two', 2), ('one', 1), ('three', 3)])
-    e = dict({'three': 3, 'one': 1, 'two': 2})
-    if a == b == c == d == e:
-        return s
-    else:
-        return False
+# def aboutDict(s):
+#     a = dict(one=1, two=2, three=3)
+#     b = {'one': 1, 'two': 2, 'three': 3}
+#     c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+#     d = dict([('two', 2), ('one', 1), ('three', 3)])
+#     e = dict({'three': 3, 'one': 1, 'two': 2})
+#     if a == b == c == d == e:
+#         return s
+#     else:
+#         return False
+
+# another solution,but use more runtime
+def isValid(self, s):
+    while '[]' in s or '()' in s or '{}' in s:
+        s = s.replace('[]', '').replace('()', '').replace('{}', '')
+    return not len(s)
