caa792b021c71c45bca5046f130542abf762d1a8
shengjia <shengj@anchnet.com>
2019-04-04 08:48:44 +0800

    single Number other solutions -- zoe

--- 136_Single_Number/singleNumber.py
+++ 136_Single_Number/singleNumber.py
@@ -1,8 +1,9 @@
 # @File    : singleNumber.py
 # @Date    : 2019-04-01
 # @Author  : shengjia
+import operator
 
-list = [2, 2, 3, 4, 1, 1, 7, -1, 7, 6, 6, 4, 3]
+list = [2, 2, 3, 4, 1, 1, 7, 5, 7, 6, 6, 4, 3]
 
 
 def singleNumber(list):
@@ -10,10 +11,8 @@ def singleNumber(list):
     result = com(list)
     while result[0] == 0:
         result = com(list)
-        if result[0] == 1:
-            return result[1]
-
-    print(result[1])
+    if result[0] == 1:
+        return result[1]
 
 
 def com(list):
@@ -28,3 +27,32 @@ def com(list):
 
 
 print(singleNumber(list))
+
+
+def singleNumber1(nums):
+    dic = {}
+    for num in nums:
+        dic[num] = dic.get(num, 0) + 1
+    for key, val in dic.items():
+        if val == 1:
+            return key
+
+
+def singleNumber2(nums):
+    res = 0
+    for num in nums:
+        res ^= num
+    return res
+
+
+def singleNumber3(nums):
+    return 2 * sum(set(nums)) - sum(nums)
+
+
+def singleNumber4(nums):
+    return reduce(lambda x, y: x ^ y, nums)
+
+
+def singleNumber(nums):
+    return reduce(operator.xor, nums)
+
