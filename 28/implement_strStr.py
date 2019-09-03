831e115b2679e964aff0be12fd4ed0c9a858c8ee
盛佳 <1368329355@qq.com>
2019-09-03 17:26:50 +0800

    ##28## -- zoe

---
+++ 28/implement_strStr.py
@@ -0,0 +1,13 @@
+# @File    : implement_strStr.py
+# @Date    : 2019-09-03
+# @Author  : shengjia
+h = 'hello'
+n = 'll2'
+
+
+def strStr(haystack, needle):
+    str = haystack.find(needle)
+    return str
+
+
+strStr(h, n)
