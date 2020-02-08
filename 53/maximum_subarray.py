bb951ffc76b5c331315cf1fb020992f332e77e1e
盛佳 <1368329355@qq.com>
2020-02-08 21:14:52 +0800

    ##53##  complete info -- zoe

--- 53/maximum_subarray.py
+++ 53/maximum_subarray.py
@@ -1,15 +1,14 @@
 # @File    : maximum_subarray.py
-# @Date    : 2019-11-08
+# @Date    : 2020-02-08
 # @Author  : shengjia
 
 def maxSubArray(nums):
     for i in range(1, len(nums)):
-        print(nums[i-1])
         if nums[i - 1] > 0:
             nums[i] += nums[i - 1]
-            print(nums[i])
-    # print(max(nums))
+    print(max(nums))
+    return max(nums)
 
 
-inputs = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+inputs = [-2, 1, -3, 4, -1, 2, 5, -5, 4]
 maxSubArray(inputs)
